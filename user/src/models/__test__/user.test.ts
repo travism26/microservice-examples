9b9b777ad8d97face2cdc5b99af4584d19a7f52e
travis martin <travismartin266@gmail.com>
2024-07-04 05:58:51 -0300

    add user model and test - I am still planning out my service might go through some changes still

---
+++ user/src/models/__test__/user.test.ts
@@ -0,0 +1,28 @@
+import { User } from '../user';
+
+it('implements optimistic concurrency control', async () => {
+    // Create an instance of a user
+    const user = User.build({
+        email: 'test@test.com',
+        firstName: 'Test',
+        lastName: 'User',
+    });
+    // Save the user to the database
+    await user.save();
+    // Fetch the user twice
+    const firstInstance = await User.findById(user.id);
+    const secondInstance = await User.findById(user.id);
+    // Make two separate changes to the users we fetched
+    firstInstance!.set({ firstName: 'New Test' });
+    secondInstance!.set({ firstName: 'Another Test' });
+    // Save the first fetched user
+    await firstInstance!.save();
+    // Save the second fetched user and expect an error
+    try {
+        await secondInstance!.save();
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+    throw new Error('Should not reach this point');
+});
