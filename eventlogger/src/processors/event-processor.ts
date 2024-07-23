a2883425beed58d067ec4372785b29e47d803653
travis martin <travismartin266@gmail.com>
2024-07-23 05:45:58 -0300

    Clean up some noisy logs in the eventlogger

--- eventlogger/src/processors/event-processor.ts
+++ eventlogger/src/processors/event-processor.ts
@@ -2,11 +2,10 @@ import { SystemEvent } from '@rickjms/microservices-common';
 import { SystemEvent as SystemEventModel } from '../models/system-event';
 
 const processEvent = async (event: SystemEvent['data']) => {
-  console.log('Event data:', event);
   const systemEvent = SystemEventModel.build({
     ...event,
   });
-  console.log('Saving event:', systemEvent);
+  console.log('Saving event:', systemEvent.eventId);
   await systemEvent.save();
   console.log('Event saved successfully');
 };
